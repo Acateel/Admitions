9356be21b51907f99ccac11a630a97a678c6d7a2
Taras Kozodoi <acateely@gmail.com>
2022-05-21 21:54:36 +0300

    add User

---
+++ src/main/java/com/adminitions/entities/users/User.java
@@ -0,0 +1,42 @@
+package com.adminitions.entities.users;
+
+import com.adminitions.entities.Entity;
+
+public class User extends Entity {
+    private String login;
+    private String password;
+    private Role role;
+    private int applicantId;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public int getApplicantId() {
+        return applicantId;
+    }
+
+    public void setApplicantId(int applicantId) {
+        this.applicantId = applicantId;
+    }
+}
