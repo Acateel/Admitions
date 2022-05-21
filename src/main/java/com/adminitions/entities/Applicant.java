fca425ac0e02ad456a1e96808e4c642085652b0b
Taras Kozodoi <acateely@gmail.com>
2022-05-21 21:49:53 +0300

    add Applicant

---
+++ src/main/java/com/adminitions/entities/Applicant.java
@@ -0,0 +1,85 @@
+package com.adminitions.entities;
+
+public class Applicant extends Entity {
+    private String lastName;
+    private String name;
+    private String surname;
+    private String email;
+    private String city;
+    private String region;
+    private String nameEducationalInstitution;
+    private String attestation; // may be change type
+    private boolean block;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getNameEducationalInstitution() {
+        return nameEducationalInstitution;
+    }
+
+    public void setNameEducationalInstitution(String nameEducationalInstitution) {
+        this.nameEducationalInstitution = nameEducationalInstitution;
+    }
+
+    public String getAttestation() {
+        return attestation;
+    }
+
+    public void setAttestation(String attestation) {
+        this.attestation = attestation;
+    }
+
+    public boolean isBlock() {
+        return block;
+    }
+
+    public void setBlock(boolean block) {
+        this.block = block;
+    }
+}
