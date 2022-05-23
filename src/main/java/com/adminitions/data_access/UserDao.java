60aea13328b0aa8136362c23931795760119d2dd
Taras Kozodoi <acateely@gmail.com>
2022-05-23 21:12:51 +0300

    add create in UserDao

--- src/main/java/com/adminitions/data_access/UserDao.java
+++ src/main/java/com/adminitions/data_access/UserDao.java
@@ -19,12 +19,21 @@ public class UserDao extends BaseDao<User> {
         );
 
         UserDao dao = new UserDao(pool);
-        System.out.println(dao.findEntityById(2));
+
+        User user = new User();
+        user.setLogin("admin");
+        user.setPassword("admin");
+        user.setRole(Role.ADMIN);
+
+        System.out.println(dao.create(user));
     }
+
     private static final String SQL_SELECT_ALL =
             "select * from `user`";
     private static final String SQL_SELECT_BY_ID =
             "select * from user where id=?";
+    private static final String SQL_INSERT =
+            "INSERT INTO `user` (login, `password`, `role`, applicant_id) VALUES (?, ?, ?, ?);";
 
     public UserDao(BasicConnectionPool connectionPool) {
         super(connectionPool);
@@ -84,7 +93,27 @@ public class UserDao extends BaseDao<User> {
 
     @Override
     boolean create(User entity) throws DaoException {
-        return false;
+        boolean createComplete;
+        Connection connection = null;
+        PreparedStatement statement = null;
+        try {
+            connection = connectionPool.getConnection();
+            statement = connection.prepareStatement(SQL_INSERT);
+
+            statement.setString(1, entity.getLogin());
+            statement.setString(2, entity.getPassword());
+            statement.setString(3, parseRoleToString(entity.getRole()));
+            statement.setInt(4, entity.getApplicantId());
+
+            int changeCount = statement.executeUpdate();
+            createComplete = changeCount > 0;
+        } catch (SQLException throwable) {
+            throw new DaoException(throwable.getMessage());
+        } finally {
+            close(statement);
+            close(connection);
+        }
+        return createComplete;
     }
 
     private User parseResultSet(ResultSet resultSet) {
@@ -100,14 +129,26 @@ public class UserDao extends BaseDao<User> {
         }
         return user;
     }
-    private Role parseRole(String strRole){
-        switch (strRole){
+
+    private Role parseRole(String strRole) {
+        switch (strRole) {
             case "admin":
                 return Role.ADMIN;
             case "applicant":
                 return Role.APPLICANT;
             default:
                 return Role.UNKNOWN;
+        }
+    }
+
+    private String parseRoleToString(Role role) throws DaoException {
+        switch (role) {
+            case ADMIN:
+                return "admin";
+            case APPLICANT:
+                return "applicant";
+            default:
+                throw new DaoException("Role not found for DB");
         }
     }
 }
