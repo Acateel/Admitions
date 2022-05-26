3721fdc85320a37222cd083ca3f39e99da75dec3
Taras Kozodoi <acateely@gmail.com>
2022-05-26 21:11:27 +0300

    add check name to validator

--- src/main/java/com/adminitions/validators/Validator.java
+++ src/main/java/com/adminitions/validators/Validator.java
@@ -1,8 +1,11 @@
 package com.adminitions.validators;
 
 public class Validator {
+    private Validator(){}
     private static final String REGEX_EMAIL = "^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$";
     private static final String REGEX_PASSWORD = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$";
+
+    private static final String REGEX_NAME = "^[^\\(\\)\\{\\}\\[\\]\\=\\+\\*\\/0-9]+$";
 
     public static boolean checkEmail(String email){
         return email.matches(REGEX_EMAIL);
@@ -10,5 +13,9 @@ public class Validator {
 
     public static boolean checkPassword(String password){
         return password.matches(REGEX_PASSWORD);
+    }
+
+    public static boolean checkName(String name){
+        return name.matches(REGEX_NAME);
     }
 }
