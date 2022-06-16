80c984cf8d4997086497e6780198235d6b01f89b
Taras Kozodoi <acateely@gmail.com>
2022-06-16 15:19:34 +0300

    change FinalizerServletTest

--- src/test/java/com/adminitions/admitions/admin/FinalizerServletTest.java
+++ src/test/java/com/adminitions/admitions/admin/FinalizerServletTest.java
@@ -2,16 +2,18 @@ package com.adminitions.admitions.admin;
 
 import com.adminitions.admitions.finalizers.Finalizer;
 import com.adminitions.data_access.DaoException;
+import com.adminitions.entities.users.Role;
+import com.adminitions.entities.users.User;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class FinalizerServletTest {
 
@@ -20,6 +22,13 @@ class FinalizerServletTest {
         FinalizerServlet servlet = new FinalizerServlet();
         HttpServletRequest request = mock(HttpServletRequest.class);
         HttpServletResponse response = mock(HttpServletResponse.class);
+        HttpSession session = mock(HttpSession.class);
+
+        User user = new User();
+        user.setRole(Role.ADMIN);
+        when(session.getAttribute("User")).thenReturn(user);
+        when(request.getSession()).thenReturn(session);
+
         Finalizer finalizer = mock(Finalizer.class);
         servlet.finalizer = finalizer;
 
